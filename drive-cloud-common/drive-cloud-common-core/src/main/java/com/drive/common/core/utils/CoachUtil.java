66b2d8b651daa65bc673718c873a78971336a9ad
古易 <18314402549>
2021-04-18 21:19:23 +0800

    添加 获取redis教练姓名 ，获取客服姓名 方法

--- drive-cloud-common/drive-cloud-common-core/src/main/java/com/drive/common/core/utils/CoachUtil.java
+++ drive-cloud-common/drive-cloud-common-core/src/main/java/com/drive/common/core/utils/CoachUtil.java
@@ -1,6 +1,8 @@
 package com.drive.common.core.utils;
 
 import cn.hutool.db.nosql.redis.RedisDS;
+import com.alibaba.fastjson.JSONObject;
+import com.drive.common.core.constant.CacheConstants;
 import com.drive.common.core.constant.Constants;
 import redis.clients.jedis.Jedis;
 
@@ -16,6 +18,39 @@ public class CoachUtil {
     public static String getAreaCoach(String key){
         return jedis.get(Constants.AREA_KEY + key);
     }
+
+
+    /**
+     * 根据教练id，获取教练姓名
+     * @param caochId  教练id
+     * @return 返回教练姓名
+     */
+    public static String getCoachName(String caochId){
+        if(StringUtils.isNotNull(caochId)){
+            String cacheRes = jedis.get(CacheConstants.REDIS_CACHE_COACH_KEY + caochId);
+            JSONObject jsonObject = (JSONObject) JSONObject.parse(cacheRes);
+            if (jsonObject != null)
+                return jsonObject.getString("realName");
+        }
+        return "";
+    }
+
+
+    /**
+     * 根据客服id，获取客服姓名
+     * @param serviceId  客服id
+     * @return 返回客服姓名
+     */
+    public static String getServiceName(String serviceId){
+        if(StringUtils.isNotNull(serviceId)){
+            String cacheRes = jedis.get(CacheConstants.REDIS_CACHE_SERVICE_KEY + serviceId);
+            JSONObject jsonObject = (JSONObject) JSONObject.parse(cacheRes);
+            if (jsonObject != null)
+                return jsonObject.getString("realName");
+        }
+        return "";
+    }
+
 
 
 }
